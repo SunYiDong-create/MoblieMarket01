1e6cdd472c70ff08dceaf8343896d2f7a867742b
SunYiDong-create <70705851+SunYiDong-create@users.noreply.github.com>
2020-09-09 18:16:21 +0800

    Update JSONUtil.java

--- MobileMarket/app/src/main/java/MoblieMarket/com/MoblieMarket/utils/JSONUtil.java
+++ MobileMarket/app/src/main/java/MoblieMarket/com/MoblieMarket/utils/JSONUtil.java
@@ -1,9 +1,4 @@
-/*
-*JSONUtil.java
-*Created on 2014-9-29 上午9:54 by Ivan
-*Copyright(c)2014 Guangzhou Onion Information Technology Co., Ltd.
-*http://www.cniao5.com
-*/
+
 package MoblieMarket.com.MoblieMarket.utils;
 
 import com.google.gson.Gson;
